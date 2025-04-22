5dd408d80ca6847bca99f5ba2a47cdade5f0f46e
ZubinSingh1 <zubinsingh10@gmail.com>
2025-04-22 13:16:27 -0400

    middleware tweak

--- Backend/api/app.py
+++ Backend/api/app.py
@@ -50,12 +50,10 @@ origins = ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:8
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_origin_regex=r"https://.*\.brikli\.com",
+    allow_origins=["https://app.brikli.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
 )
 
 
